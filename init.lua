30abc74a4c71986bf89a73df3ed1d8b67b87a11d
Joshua Jones <joshua@general-metrics.com>
2022-01-02 16:47:39 -0800

    Rebuild Lua config from scratch, starting with iterative steps.

--- init.lua
+++ init.lua
@@ -1,19 +1,19 @@
 -- General
-require("settings")
-require("mappings")
+require("user.options")
+require("user.mappings")
 
 -- Plugins
-require("plugins")
-require("colorizer").setup()
+require("user.plugin-manager")
+-- require("colorizer").setup()
 
 -- LSP
-require("treesitter")
-require("lsp-config")
-require("completion")
-require("lspkind").init()
-require("lspsaga").init_lsp_saga()
--- require "lsp-install"
--- require "nvim-dap"
+-- require("treesitter")
+-- require("lsp-config")
+require("user.cmp")
+-- require("lspkind").init()
+-- require("lspsaga").init_lsp_saga()
+-- -- require "lsp-install"
+-- -- require "nvim-dap"
 
--- ColorsScheme
+-- -- ColorsScheme
 require("themes/dracula")
