3530f54a13101a077e8a2de9b4102be4c5ef33c5
Joshua Jones <joshua@general-metrics.com>
2024-09-10 10:29:58 -0700

    Update TS LSP key.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -173,7 +173,7 @@ return {
       })
 
       -- TypeScript
-      lspconfig.tsserver.setup({
+      lspconfig.ts_ls.setup({
         capabilities = capabilities,
         on_attach = on_attach,
         settings = {
