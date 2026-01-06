92af84b87e7942fdae494bae9cdb3af64f0a5e2a
Joshua Jones <joshua@general-metrics.com>
2026-01-05 16:31:19 -0800

    Add LSP, formatting support.

--- README.md
+++ README.md
@@ -25,3 +25,42 @@ environment and how it works so I can hone its functionality even tighter to my 
 Keymaps is another one that is pretty self-explanitory from reading each remap,
 though here I like to group each remap by what it does with some heading
 comments.
+
+### Plugins
+
+I switched to `vim.pack` for native plugin handling.
+
+It's easy but there are little implementation details I found I needed to be
+aware of. Overall being aware that `PackChanged` events don't run on install,
+because Lua is a scripting language, you can just run post-install hooks simply
+after the import is declared.
+
+##### Treesitter
+
+This is important: In `0.12` make sure you assign `nvim-treesitter` and
+`nvim-treesitter-textobjects` to `version = "main"`. Requiring in the `main`
+branch is slightly different, `require("nvim-treesitter")` instead of
+`require("nvim-treesitter.configs")`.
+
+##### Mini
+
+Super easy, just add and require.
+
+##### LSP
+
+This part is more involved and will likely require more configuration
+involvement. Main layout looks like this:
+
+```lua
+vim.pack.add({ "https://github.com/mason-org/mason.nvim" })
+require("mason").setup()
+
+vim.lsp.enable("lua_ls")
+vim.lsp.config("lua_ls", { ... })
+
+-- keymaps
+kset("n", "gd", vim.lsp.buf.definition, noremap_opts)
+kset("n", "grt", vim.lsp.buf.type_definition, noremap_opts)
+kset("n", "rn", vim.lsp.buf.rename, noremap_opts)
+```
+
--- init.lua
+++ init.lua
@@ -1,11 +1,20 @@
 local COLUMN_WIDTH = 80
 
+-- Functions
+local kset = vim.keymap.set
+local noremap_opts = { silent = true, noremap = true }
+local ptable = function(t)
+  for key, value in pairs(t) do
+    print(key, value)
+  end
+end
+
 --[[
 -- Global settings 
 --]]
 
-vim.g.mapleader = " " 
-vim.g.maplocalleader = " " 
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 --[[
 -- Options 
@@ -33,7 +42,7 @@ vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
-vim.o.smarttab = true  
+vim.o.smarttab = true
 
 -- Display
 vim.o.termguicolors = true
@@ -43,15 +52,234 @@ vim.o.splitright = true
 -- Window Options
 vim.wo.number = true
 vim.wo.relativenumber = true
-vim.wo.signcolumn = "yes" -- Always add a little buffer for any signs to left 
+vim.wo.signcolumn = "yes" -- Always add a little buffer for any signs to left
 vim.wo.colorcolumn = tostring(COLUMN_WIDTH) -- Display the column vertical line
 
---[
--- Keymaps 
---]
-local kset = vim.keymap.set
-local noremap_opts = { silent = true, noremap = true }
+-- Highlight on yank
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", {
+  clear = true,
+})
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = "*",
+})
+
+--[[
+-- Plugins
+--]]
+local gh = function(package)
+  return "https://github.com/" .. package
+end
+
+-- Colorscheme
+vim.pack.add({ gh("eldritch-theme/eldritch.nvim") }, { confirm = false })
+vim.cmd.colorscheme("eldritch-dark")
+
+-- Treesitter
+vim.api.nvim_create_autocmd("PackChanged", {
+  desc = "Handle nvim-treesitter updates",
+  group = vim.api.nvim_create_augroup("nvim-treesitter-pack-changed-update-handler", { clear = true }),
+  callback = function(event)
+    if event.data.kind == "update" and event.data.spec.name == "nvim-treesitter" then
+      vim.notify("nvim-treesitter updated, running TSUpdate...", vim.log.levels.INFO)
+      ---@diagnostic disable-next-line: param-type-mismatch
+      local ok = pcall(vim.cmd, "TSUpdate")
+
+      if ok then
+        vim.notify("TSUpdate completed successfiully", vim.log.levels.INFO)
+      else
+        vim.notify("TSUpdate command not available yet, skipping...", vim.log.levels.WARN)
+      end
+    end
+  end,
+})
+
+vim.pack.add({
+  {
+    src = gh("nvim-treesitter/nvim-treesitter"),
+    version = "main",
+  },
+  {
+    src = gh("nvim-treesitter/nvim-treesitter-textobjects"),
+    version = "main",
+  },
+}, { load = true })
+
+require("nvim-treesitter").setup({
+  auto_install = true,
+  sync_install = true,
+  ensure_installed = {
+    "c",
+    "elm",
+    "go",
+    "hare",
+    "html",
+    "javascript",
+    "json",
+    "lua",
+    "markdown",
+    "ocaml",
+    "python",
+    "rust",
+    "swift",
+    "templ",
+    "tsx",
+    "typescript",
+    "vimdoc",
+    "vim",
+    "zig",
+  },
+})
+
+-- Mini
+vim.pack.add({ gh("nvim-mini/mini.nvim") })
+
+require("mini.comment").setup({
+  mappings = {
+    comment = "<leader>/",
+    comment_line = "<leader>/",
+    comment_visual = "<leader>/",
+  },
+})
+require("mini.files").setup({})
+require("mini.icons").setup()
+require("mini.notify").setup()
+require("mini.pairs").setup()
+require("mini.pick").setup()
+require("mini.statusline").setup()
+require("mini.surround").setup()
+
+local pick_files = function()
+  MiniPick.builtin.files({ tool = "git" })
+end
+
+kset("n", "<leader>bb", MiniPick.builtin.buffers, noremap_opts)
+kset("n", "<leader>ff", pick_files, noremap_opts)
+kset("n", "<leader>fh", MiniPick.builtin.help, noremap_opts)
+kset("n", "<leader>e", MiniFiles.open, noremap_opts)
+
+-- LSP
+vim.pack.add({
+  gh("neovim/nvim-lspconfig"),
+  gh("mason-org/mason.nvim"),
+})
+require("mason").setup()
+
+-- Enable LSP if in Git repo
+vim.lsp.config("*", { root_markers = { ".git" } })
+
+local lsps = {
+  { "cssls" },
+  {
+    "eslint",
+  },
+  { "gopls" },
+  {
+    "lua_ls",
+    {
+      settings = {
+        Lua = {
+          runtime = { version = "LuaJIT" },
+          workspace = { checkThirdParty = false },
+          telemetry = { enable = false },
+          completion = {
+            callSnippet = "Replace",
+          },
+        },
+      },
+    },
+  },
+  {
+    "ts_ls",
+  },
+}
+
+for _, lsp in pairs(lsps) do
+  local name, config = lsp[1], lsp[2]
+
+  vim.lsp.enable(name)
+
+  if config then
+    vim.lsp.config(name, config)
+  end
+end
+
+-- Diagostics
+vim.diagnostic.config({
+  virtual_text = true,
+  underline = true,
+})
+
+kset("n", "gd", vim.lsp.buf.definition, noremap_opts)
+kset("n", "grt", vim.lsp.buf.type_definition, noremap_opts)
+kset("n", "rn", vim.lsp.buf.rename, noremap_opts)
+
+--[
+-- Conform - Formatting
+--]
+vim.pack.add({ gh("stevearc/conform.nvim") })
+
+local conform = require("conform")
+
+---@param bufnr integer
+---@param ... string
+---@return string
+local function first(bufnr, ...)
+  for i = 1, select("#", ...) do
+    local formatter = select(i, ...)
+    if conform.get_formatter_info(formatter, bufnr).available then
+      return formatter
+    end
+  end
+  return select(1, ...)
+end
+
+conform.setup({
+  formatters_by_ft = {
+    bash = { "shellharden" },
+    c = { "clang-format" },
+    css = function(bufnr)
+      return { first(bufnr, "prettierd", "prettier", "biome", "stylelint") }
+    end,
+    go = { "goimports", "gofmt" },
+    html = function(bufnr)
+      return { first(bufnr, "biome", "prettier", "prettierd") }
+    end,
+    -- NOTE: use sublist to pick biome first
+    javascript = function(bufnr)
+      return { first(bufnr, "prettier", "prettierd", "biome", "deno_fmt") }
+    end,
+    json = { "prettierd", "prettier" },
+    liquid = { "prettierd", "prettier" },
+    lua = { "stylua" },
+    markdown = function(bufnr)
+      -- return { first(bufnr, "deno_fmt", "prettierd", "prettier"), "injected" }
+      return { first(bufnr, "deno_fmt", "prettierd", "prettier") }
+    end,
+    nix = { "alejandra" },
+    php = function(bufnr)
+      return { first(bufnr, "pint", "pretty-php") }
+    end,
+    scss = function(bufnr)
+      return { first(bufnr, "prettierd", "prettier", "stylelint") }
+    end,
+    sh = { "shellharden" },
+    templ = { "templ" },
+  },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
+})
+
+--[
+-- Global Keymaps
+--]
 kset({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 kset("n", "<leader>rl", ":so %<CR>", noremap_opts)
 kset("n", "<leader>q", ":qa<CR>", noremap_opts)
@@ -90,17 +318,3 @@ kset("v", "p", '"_dP', noremap_opts)
 
 -- Open scratch buffer
 kset("n", "<leader>n", ":new<CR>", noremap_opts)
-
--- Highlight on yank
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { 
-  clear = true
-})
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = "*",
-})
